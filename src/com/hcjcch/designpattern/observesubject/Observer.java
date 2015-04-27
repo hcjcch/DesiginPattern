f2e2127cfe993702bd0cf54bfd0f39d10abe76f3
hcjcch <huangchen@mail.bistu.edu.cn>
2015-04-27 16:06:49 +0800

    add observe subject

---
+++ src/com/hcjcch/designpattern/observesubject/Observer.java
@@ -0,0 +1,9 @@
+package com.hcjcch.designpattern.observesubject;
+
+public interface Observer {
+    /**
+     * 更新接口
+     * @param state    更新的状态
+     */
+    public void update(String state);
+}
--- src/com/hcjcch/designpattern/templatedesignpatten/test2/CDAccount.java
+++ src/com/hcjcch/designpattern/templatedesignpatten/test2/CDAccount.java
@@ -1,6 +1,5 @@
 package com.hcjcch.designpattern.templatedesignpatten.test2;
 
-import com.hcjcch.designpattern.templatedesignpatten.test2.Account;
 
 /**
  * Created by hcjcch on 2015/4/19.
