2c5aee25265da329d9b73733fbe9d56a3de35bbb
hcjcch <huangchen@mail.bistu.edu.cn>
2015-04-29 17:05:45 +0800

    add proxy

---
+++ src/com/hcjcch/designpattern/proxy/AbstractObject.java
@@ -0,0 +1,6 @@
+package com.hcjcch.designpattern.proxy;
+
+public abstract class AbstractObject {
+    //²Ù×÷
+    public abstract void operation();
+}
---
+++ src/com/hcjcch/designpattern/proxy/RealObject.java
@@ -0,0 +1,10 @@
+
+package com.hcjcch.designpattern.proxy;
+
+public class RealObject extends AbstractObject {
+    @Override
+    public void operation() {
+        //一些操作
+        System.out.println("一些操作");
+    }
+}
